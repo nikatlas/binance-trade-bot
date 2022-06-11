695164121e6ee4c3e708ac0e42d0ac3784858831
Nikos Atlas <nikatlas@gmail.com>
2022-06-11 19:52:41 +0300

    format

--- binance_trade_bot/__init__.py
+++ binance_trade_bot/__init__.py
@@ -1,3 +1,5 @@
 from .backtest import backtest
 from .binance_api_manager import BinanceAPIManager
 from .crypto_trading import main as run_trader
+
+__all__ = ["backtest", "BinanceAPIManager", "run_trader"]
